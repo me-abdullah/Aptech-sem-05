e6c3b905a237a9711575c605104f9177be105335
me-abdullah <enzoemrick@gmail.com>
2024-12-22 15:22:44 +0500

    Express

---
+++ Node & Express Learning/Class 12/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(function (req, res,next) {
+    console.log('Hello World');
+    next();
+  });
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
+app.get('/profile', function (req, res) {
+    res.send('Profile Page')
+  });
+  app.get('/dashboard', function (req, res, next) {
+    return next(new Error("Something Went Wrong"))
+  });
+  app.use((err,req,res,next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  })
+
+app.listen(3000)
