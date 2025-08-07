b901c8daa5a98bf43f289efe7024bfb74031136f
Coder-Ayesha <easycode170@gmail.com>
2025-08-07 16:21:55 +0500

    updated class 2

--- React Learning/Class 02/src/App.js
+++ React Learning/Class 02/src/App.js
@@ -1,24 +1,12 @@
+import React from 'react'
+import UsersList from './UserList'
+import Examp from './Examp'
 
-import './App.css';
-
-import React, { useState, useEffect } from 'react';
-
-function App() {
-  const [count, setCount] = useState(0); // Initializing state with 0
-
-  useEffect(() => {
-    console.log('useEffect is running with count:', count);
-    document.title = `You clicked ${count} times`;
-  }, []);
-
-
+const App = () => {
   return (
-    <div>
-      <h1> {count} </h1>
-      <button type='button' className='btn btn-primary' onClick={() => setCount(count + 1)}>Increament</button>
-      <button type='button' className='btn btn-secondary' onClick={() => setCount(count - 1)}>Dcreament</button>
-    </div>
-  );
+    // <UsersList/>
+    <Examp/>
+  )
 }
 
-export default App;
+export default App
